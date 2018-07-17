1a48c262e3455fcfd5c9b6316d402d0e589ca139
Mark Otway <mark@otway.com>
2018-07-17 16:53:48 +0100

    Adjust size parameter which speeds up conversion by a factor of 8. Also, support for graphicsmagick

--- Program.cs
+++ Program.cs
@@ -10,6 +10,7 @@ namespace SynoThumbnailGen
     {
         private class SynoThumb
         {
+            public bool useAsSource;
             public int width;
             public int height;
             public string fileFormatString;
@@ -19,21 +20,30 @@ namespace SynoThumbnailGen
         /// This is the set of thumb resolutions that Syno PhotoStation and moments expects
         /// </summary>
         private static SynoThumb[] thumbConfigs = {
-            new SynoThumb{ width = 1280, height = 1280, fileFormatString = "{0}/@eaDir/{1}/SYNOPHOTO_THUMB_XL.jpg" },
-            new SynoThumb{ width = 800, height = 800, fileFormatString = "{0}/@eaDir/{1}/SYNOPHOTO_THUMB_L.jpg" },
+            new SynoThumb{ width = 1280, height = 1280, fileFormatString = "{0}/@eaDir/{1}/SYNOPHOTO_THUMB_XL.jpg", useAsSource = true },
+            new SynoThumb{ width = 800, height = 800, fileFormatString = "{0}/@eaDir/{1}/SYNOPHOTO_THUMB_L.jpg", useAsSource = true },
             new SynoThumb{ width = 640, height = 640, fileFormatString = "{0}/@eaDir/{1}/SYNOPHOTO_THUMB_B.jpg" },
             new SynoThumb{ width = 320, height = 320, fileFormatString = "{0}/@eaDir/{1}/SYNOPHOTO_THUMB_M.jpg" },
+            new SynoThumb{ width = 160, height = 120, fileFormatString = "{0}/@eaDir/{1}/SYNOPHOTO_THUMB_PREVIEW.jpg" },
             new SynoThumb{ width = 120, height = 120, fileFormatString = "{0}/@eaDir/{1}/SYNOPHOTO_THUMB_S.jpg" }
         };
 
         private static string[] extensions = { ".jpg", ".jpeg" };
         private static bool s_verbose = false;
         private static bool s_alphaSort = false;
+        private static bool s_useGraphicsMagick = false;
 
         private static void Usage()
         {
+
             Console.WriteLine("Usage: ThumbnailGen src");
             Console.WriteLine("Generates Synology thumbnails in the within the specified folder.");
+        }
+
+        public static void LogVerbose(string format, params object[] args)
+        {
+            if (s_verbose)
+                Log(format, args);
         }
 
         public static void Log(string format, params object[] args)
@@ -92,18 +102,23 @@ namespace SynoThumbnailGen
 
             int total = files.Count();
             int current = 0;
+            int converted = 0;
 
             foreach (var f in files)
             {
                 current++;
 
-                if (s_verbose)
-                    Log("Processing ({0} of {1}) - {2} into {3} for {4} sizes.", current, total, f.FullName, f.FullName, thumbConfigs.Length);
-                else
-                    Log("Processing ({0} of {1}) {2}.", current, total, f.FullName);
+                LogVerbose("Analysing ({0} of {1}) - {2} into {3} for {4} sizes.", current, total, f.FullName, f.FullName, thumbConfigs.Length);
 
-                ConvertFile(f, thumbConfigs);
+                if (ConvertFile(f, thumbConfigs))
+                {
+                    Log("Converted ({0} of {1}) {2}.", current, total, f.FullName);
+                    converted++;
+                }
+
             }
+
+            Log("Folder complete. {0} of {1} files had thumbnails generated.", converted, total);
         }
 
         /// <summary>
@@ -116,45 +131,80 @@ namespace SynoThumbnailGen
         /// </summary>
         /// <param name="source">Source.</param>
         /// <param name="sizes">Sizes.</param>
-        private static void ConvertFile(FileInfo source, SynoThumb[] sizes)
+        /// <returns>True if the file was converted</returns>
+        private static bool ConvertFile(FileInfo source, SynoThumb[] sizes)
         {
+            bool conversionPerformed = false;
+
             // Some useful unsharp and quality settings, plus by defining the max size of the JPEG, it 
             // makes imagemagic more efficient with its memory allocation, so significantly faster. 
-            string args = string.Format(" -define jpeg:size=3000x2250 \"{0}\" -quality 90 -unsharp 0.5x0.5+1.25+0.0 ", source.FullName);
-            bool workToDo = false;
+            string args;
+
+            if( s_useGraphicsMagick )
+                args = string.Format(" convert -size 1280x1280 \"{0}\" -quality 90 -unsharp 0.5x0.5+1.25+0.0 ", source.FullName);
+            else
+                args = string.Format(" -define jpeg:size=1280x1280 \"{0}\" -quality 90 -unsharp 0.5x0.5+1.25+0.0 ", source.FullName);
+
             List<string> destinationFiles = new List<string>();
+            FileInfo altSource = null;
+
+            List<string> argsList = new List<string>();
 
             // First pre-check whether the thumbs exist - don't want to create them if they don't.
-            foreach (var size in sizes)
+            foreach( var size in sizes )
             {
                 string destFile = string.Format(string.Format(size.fileFormatString, source.DirectoryName, source.Name));
 
                 string destDir = Path.GetDirectoryName(destFile);
                 if (!Directory.Exists(destDir))
                 {
-                    if( s_verbose)
-                        Log("Creating directory: {0}", destDir);
+                    LogVerbose("Creating directory: {0}", destDir);
                     Directory.CreateDirectory(destDir);
                 }
 
                 if( File.Exists(destFile) && File.GetLastWriteTime( destFile) == source.LastWriteTime )
                 {
                     // If the creation time of both files is the same, we're done.
-                    if (s_verbose)
-                        Log("File {0} already exists with matching creation time.", destFile);
+                    LogVerbose("File {0} already exists with matching creation time.", destFile);
+
+                    if (altSource == null && size.useAsSource)
+                        altSource = new FileInfo(destFile);
+
                     continue; // Don't re-gen it, we're done
                 }
 
                 // File didn't exist, so add it to the command-line. 
-                args += string.Format(" -resize {0}x{1}> -auto-orient -write \"{2}\" ", size.height, size.width, destFile);
+                if( s_useGraphicsMagick )
+                    argsList.Add( string.Format(" -thumbnail {0}x{1}> -auto-orient -write \"{2}\" ", size.height, size.width, destFile) );
+                else
+                    argsList.Add( string.Format(" -resize {0}x{1}> -auto-orient -write \"{2}\" ", size.height, size.width, destFile) );
+
                 destinationFiles.Add( destFile );
-                workToDo = true;
             }
 
-            if( workToDo )
+            if( argsList.Any() )
             {
+                var lastArg = argsList.Last();
+                lastArg = lastArg.Replace(" -write ", " ");
+                argsList[argsList.Count() - 1] = lastArg;
+
+                args += string.Join(" ", argsList);
+
+                if (altSource != null)
+                {
+                    source = altSource;
+                    LogVerbose("File {0} exists - using it as source for smaller thumbs.", altSource.Name);
+                }
+
+                LogVerbose("Converting file {0}", source);
+
                 Process process = new Process();
-                process.StartInfo.FileName = "convert";
+
+                if( s_useGraphicsMagick)
+                    process.StartInfo.FileName = "gm";
+                else
+                    process.StartInfo.FileName = "convert";
+
                 process.StartInfo.Arguments = args;
                 process.StartInfo.RedirectStandardError = true;
                 process.StartInfo.RedirectStandardOutput = true;
@@ -164,10 +214,7 @@ namespace SynoThumbnailGen
 
                 try
                 {
-                    if (s_verbose)
-                        Log("  Executing: {0} {1}", process.StartInfo.FileName, process.StartInfo.Arguments);
-                    else 
-                        Log("  Executing ImageMagic: {0}", process.StartInfo.FileName);
+                    LogVerbose("  Executing: {0} {1}", process.StartInfo.FileName, process.StartInfo.Arguments);
 
                     bool success = process.Start();
 
@@ -177,13 +224,14 @@ namespace SynoThumbnailGen
                         process.BeginOutputReadLine();
                         process.WaitForExit();
 
-                        if( s_verbose)
-                            Log("Execution complete.");
+                        LogVerbose("Execution complete.");
+                        conversionPerformed = true;
 
                         // Touch the files so they match the source
                         foreach( string f in destinationFiles )
                         {
-                            File.SetLastWriteTime(f, source.LastWriteTime);
+                            if( File.Exists( f ))
+                                File.SetLastWriteTime(f, source.LastWriteTime);
                         }
                     }
                 }
@@ -193,7 +241,9 @@ namespace SynoThumbnailGen
                 }
             }
             else
-                Log("Thumbs already exist in all resolutions. Skipping...");
+                LogVerbose("Thumbs already exist in all resolutions. Skipping...");
+
+            return conversionPerformed;
         }
 
         static void Process_OutputDataReceived(object sender, DataReceivedEventArgs e)
@@ -202,7 +252,7 @@ namespace SynoThumbnailGen
                 return;
 
             if (!string.IsNullOrEmpty(e.Data))
-                Log(e.Data);
+                LogVerbose(e.Data);
         }
 
         public static void Main(string[] args)
@@ -227,13 +277,20 @@ namespace SynoThumbnailGen
                 Log("Alphabetic sort enabled.");
                 s_alphaSort = true;
             }
-
+            else
+                Log("Most-recent-first sort enabled.");
+                
             if (args.Any(x => x.ToLower() == "-v"))
             {
                 Log("Verbose mode enabled.");
                 s_verbose = true;
             }
 
+            if (args.Any(x => x.ToLower() == "-gm"))
+            {
+                Log("GraphicsMagick enabled.");
+                s_useGraphicsMagick = true;
+            }
             var root = new DirectoryInfo(rootFolder);
 
             Log("Starting Synology thumbnail creation for folder {0}", root);
